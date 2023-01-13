1941c1c6cf979add3e9843426c02d67558c75bc3
emmanuelguty <manuel15901@gmail.com>
2023-01-13 12:42:11 -0600

    Se termino el menu especial, y se corrigieron errores de estilo

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -2,10 +2,40 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
-  </div>
+// Por la falta de un div no se podia agregar los tags abajo
+const MenuItem = ({ title, price, tags }) => (
+  <div className='app__menuItem'>
+    <div className="app__menuItem-head">
+      <div className="app__menuItem-name">
+        <p className="p__cormorant" style={{ color: '#DCCA87' }}>{title}</p>
+      </div>
+
+      <div className="app__menuItem-dash" />
+
+      <div className='app__menuItem-price'>
+        <p className='p__cormorant' style={{ color: '#DCCA87' }}>{price}</p>
+      </div>
+
+      </div>
+
+      <div className='app__menuItem-sub'>
+        <p className='p__opensans' style={{ color: '#AAA' }}>{tags}</p>
+      </div>
+
+    </div>
 );
 
 export default MenuItem;
+
+//       <div className="app__menuitem-price">
+//         <p className="p__cormorant">{price}</p>
+//       </div>
+//     </div>
+
+//     <div className="app__menuitem-sub">
+//       <p className="p__opensans" style={{ color: '#AAAAAA' }}>{tags}</p>
+//     </div>
+//   </div>
+// );
+
+// export default MenuItem;
